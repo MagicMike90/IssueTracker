ddc93131641c96e7ec9f90ed6511286dd6638c52
Michael Luo <mluo@Michaels-MacBook-Pro.local>
2017-07-03 17:00:39 +1000

    add new issue

--- server.js
+++ server.js
@@ -20,6 +20,38 @@ const issues = [
         title: 'Missing bottom border on panel',
     },
 ];
+const validIssueStatus = {
+    New: true,
+    Open: true,
+    Assigned: true,
+    Fixed: true,
+    Verified: true,
+    Closed: true,
+};
+const issueFieldType = {
+    id: 'required',
+    status: 'required',
+    owner: 'required',
+    effort: 'optional',
+    created: 'required',
+    completionDate: 'optional',
+    title: 'required',
+}
+function validateIssue(issue) {
+    for (const field in issueFieldType) {
+        const type = issueFieldType[field];
+        if (!type) {
+            delete issue[field];
+        } else if (type === 'required' && !issue[field]) {
+            return `${filed} is required.`;
+        }
+    }
+    if (!validIssueStatus[issue.status])
+        return `${issue.status} is not a valid status.`;
+
+    return null;
+}
+
 app.get('/api/issues', (req, res) => {
     const metadata = { total_count: issues.length };
     res.json({ _metadata: metadata, records: issues });

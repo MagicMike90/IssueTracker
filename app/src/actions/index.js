cb6776a6b895e6f0a9373c8d374cc88e21391aa3
Michael Luo <mluo@Michaels-MacBook-Pro.local>
2017-07-18 16:33:55 +1000

    update action fetch method

--- app/src/actions/index.js
+++ app/src/actions/index.js
@@ -8,25 +8,50 @@ export const addTodo = (text) => ({
 
 export const addIssue = issue => ({
   type: types.ADD_ISSUE,
-  issue:issue
+  issue: issue
 })
 const receiveIssues = issues => ({
   type: types.RECEIVE_ISSUES,
   issues: issues
 })
 
-export const getAllIssues = () => dispatch => {
-  fetch(`/api/issues`).then(response => {
+export const getAllIssues = (location, page_size) => dispatch => {
+  const query = Object.assign({}, queryString.parse(location.search));
+  const pageStr = query._page;
+  if (pageStr) {
+    delete query._page;
+    query._offset = (parseInt(pageStr, 10) - 1) * page_size;
+  }
+  query._limit = page_size;
+
+  const search = Object.keys(query).map(k => `${k}=${query[k]}`).join('&');
+  return fetch(`/api/issues?${search}`).then(response => {
     if (!response.ok) return response.json().then(error => Promise.reject(error));
-    return response.json().then(data => ({
-      IssueList: data
-    }));
+    return response.json().then(data => dispatch(receiveIssues));
   });
 }
 
+export const requestPosts = reddit => ({
+  type: REQUEST_POSTS,
+  reddit
+})
+
+export const receivePosts = (reddit, json) => ({
+  type: RECEIVE_POSTS,
+  reddit,
+  posts: json.data.children.map(child => child.data),
+  receivedAt: Date.now()
+})
+
+const fetchPosts = reddit => dispatch => {
+  dispatch(requestPosts(reddit))
+  return fetch(`https://www.reddit.com/r/${reddit}.json`)
+    .then(response => response.json())
+    .then(json => dispatch(receivePosts(reddit, json)))
+}
 
 
-const shouldFetchIssues = (state, reddit) => {
+const shouldFetchIssues = (state, issue) => {
   const issues = state.postsByReddit[reddit]
   if (!issues) {
     return true
